d1eca305b83358bcd0ecf1a6ff25dd1db9545c33
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-11-13 22:45:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,19 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+  
+  ]
+  
+  board = ["X", "X", "X", "O", " ", "O", "O", " ", " "]
+  camel = [8, " ", " ", " ", 8, " ", " ", " ", 8]
+  
+  
